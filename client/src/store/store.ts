46e9e661a368dafc348ec2fd6fd4731056f62d6a
Vsevolod <fromdaytoday@icloud.com>
2020-10-12 18:05:12 +0300

    reformat store.ts

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,19 +1,19 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import thunkMiddleware from 'redux-thunk'
 
-import categoriesReducer from "./categories-reducer";
+import categoriesReducer from './categories-reducer'
 import booksReducer from './books-reducer'
 
 const rootReducer = combineReducers({
 	categories: categoriesReducer,
 	books: booksReducer
-});
+})
 
 type RootReducerType = typeof rootReducer;
 export type AppStateType = ReturnType<RootReducerType>
-export type InferActionsTypes<T> = T extends {[key: string]: (...args: any[]) => infer U} ? U : never
+export type InferActionsTypes<T> = T extends { [key: string]: (...args: any[]) => infer U } ? U : never
 export type Override<T1, T2> = Omit<T1, keyof T2> & T2;
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
-export default store;
+export default store
